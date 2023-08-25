15a8202bd8c31cce047246f6459e8bdfd535eaa3
zbfeitian <143128334+zbfeitian@users.noreply.github.com>
2023-08-25 23:44:37 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,12 +13,12 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi，我是一名侦察兵, Hi，我是一名探路者,Hi，恭喜你发现了新大陆',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi，我的朋友，恭喜你发现了新大陆！',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '山高人为峰', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '黑科技痴汉', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '专注钻研互联网黑科技', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://kjfeng.vip', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '微商工具, 直播助手，黑科技', // 网站关键词 英文逗号隔开
 
